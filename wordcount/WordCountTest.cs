280a3c058d235cf36c70b52128a5789af1ff7638
svenk <svenk@CPKUEW10>
2017-01-31 08:27:39 +0100

    Add project files.

---
+++ wordcount/WordCountTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace wordcount
+{
+    [TestFixture]
+    public class WordCountTest
+    {
+        [TestCase("A AB BCD", new[] { "A", "AB", "BCD" })]
+        [TestCase("A  !-? $", new[] { "A" })]
+        [TestCase("A!B", new[] { "A", "B" })]
+        public void ExtractWords(string input, string[] expected)
+        {
+            var actual = Program.ExtractWords(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void RemoveStopwords()
+        {
+            var words = new[] {"word", "stopword", "1"};
+            var stopwords = new[] {"stopword", "2"};
+            var expected = new[] {"word", "1"};
+            var actual = Program.RemoveStopwords(words, stopwords);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
